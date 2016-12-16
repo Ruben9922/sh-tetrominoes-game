8ae27c673ac08700252fa90db3aaa178f98bf263
Ruben9922 <Ruben9922@users.noreply.github.com>
2016-12-16 16:16:03 +0000

    Add ability to display and clear shapes

--- tetrominoes-game.py
+++ tetrominoes-game.py
@@ -1,11 +1,24 @@
 from sense_hat import SenseHat
 from time import sleep
 
+def display_shape(shape, originX, originY, clear = False):
+    for i in range(len(shape)):
+        for j in range(len(shape[i])):
+            if (shape[i][j]):
+                x = originX + j
+                y = originY + i
+                sense.set_pixel(x, y, background_colour if clear else shape_colour)
+
+def clear_shape(shape, originX, originY):
+    display_shape(shape, originX, originY, True)
+
 sense = SenseHat()
 sense.set_rotation(180)
+sense.clear() # Temporarily moved this for quicker testing
 sense.low_light = True
 
 shape_colour = [255, 153, 51]
+background_colour = [0, 0, 0]
 shapes = [
     [
         [True, True, True, True]
@@ -36,5 +49,10 @@ shapes = [
     ]
 ]
 
-sense.clear()
+for i in range(len(shapes)):
+    display_shape(shapes[i], 0, 0)
+    sleep(0.5)
+    clear_shape(shapes[i], 0, 0)
+    sleep(0.1)
+
 sense.low_light = False
